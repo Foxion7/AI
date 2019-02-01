998ac40d223130dd388581b69ebee0b5a54d3df9
hassanAlAwwadi <hassan.awwadi@gmail.com>
2019-02-02 00:12:47 +0100

    added neighbor detection

--- AI_App/SteeringCS/behaviour/FlockBehaviour.cs
+++ AI_App/SteeringCS/behaviour/FlockBehaviour.cs
@@ -21,10 +21,20 @@ namespace SteeringCS.behaviour
 
         public Vector2D Calculate()
         {
-            var group = ME.Neighbors;
+            var group = ME.Neighbors.Where(gob =>
+            {
+                var x = gob.Pos.X;
+                var y = gob.Pos.Y;
+                var maxX = ME.Pos.X + ME.NeighborsRange;
+                var minX = ME.Pos.X - ME.NeighborsRange;
+                var maxY = ME.Pos.X + ME.NeighborsRange;
+                var minY = ME.Pos.X - ME.NeighborsRange;
+                return x > minX && x < maxX && y > minY && y < maxY;
+            });
             var groupL= group as List<IMover> ?? group.ToList();
             if (!groupL.Any())
                 return new Vector2D();
+
 
             var separationForce =  Separation(groupL, ME) * ME.SeparationValue;
             var alignmentForce = Alignment(groupL)        * ME.AlignmentValue;

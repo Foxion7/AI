2bc3f215d234cf2dede1bdd12f25d9c7b9bdd00d
woeffie <roeland.steam@gmail.com>
2019-02-27 13:13:48 +0100

    Added working hobgoblin command 0: Gather.

--- AI_App/SteeringCS/States/GoblinState/Obey.cs
+++ AI_App/SteeringCS/States/GoblinState/Obey.cs
@@ -19,19 +19,50 @@ namespace SteeringCS.States.GoblinState
 
         public void Act(float timeElapsed)
         {
-            if(goblin.Commander.CurrentCommand == 0)
+            // Approach commander.
+            if (goblin.Commander.CurrentCommand == 0)
             {
-                goblin.FollowingOrder = true;
-                goblin.Target = goblin.Commander;
+                Vector2D steeringForce = new Vector2D(0, 0);
+
+                if (goblin._SB != null)
+                    steeringForce += goblin._SB.Calculate() * 4;
+                if (goblin._FlockB != null)
+                    steeringForce += goblin._FlockB.Calculate() * 0.5;
+                if (goblin._OA != null)
+                    steeringForce += goblin._OA.Calculate();
+                if (goblin._WA != null)
+                    steeringForce += goblin._WA.Calculate();
+                steeringForce.Truncate(goblin.MaxForce);
+
+                Vector2D acceleration = steeringForce / goblin.Mass;
+
+                goblin.Velocity += (acceleration * timeElapsed);
+                goblin.Velocity = goblin.Velocity.Truncate(goblin.MaxSpeed);
+                goblin.OldPos = goblin.Pos;
+                goblin.Pos += (goblin.Velocity * timeElapsed);
+                if (goblin.Velocity.LengthSquared() > 0.00000001)
+                {
+                    goblin.Heading = goblin.Velocity.Normalize();
+                    goblin.Side = goblin.Heading.Perp();
+                }
+                goblin.WrapAround();
+                goblin.world.rePosGoblin(goblin.Key, goblin.OldPos, goblin.Pos);
             }
         }
 
         public void Enter( )
         {
+            if (goblin.Commander.CurrentCommand == 0)
+            {
+                goblin.Target = goblin.Commander;
+
+            }
+            goblin.FollowingOrder = true; // Maybe to be removed
         }
 
         public void Exit( )
         {
+            goblin.FollowingOrder = false; // Maybe to be removed
         }
 
         public override string ToString()

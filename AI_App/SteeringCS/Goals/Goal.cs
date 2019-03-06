75f2eed4550708cc277e203d5feefd663686013c
woeffie <roeland.steam@gmail.com>
2019-03-06 13:27:54 +0100

    Added parts of goal stuff

---
+++ AI_App/SteeringCS/Goals/Goal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteeringCS.Goals
+{
+    public class Goal : GoalComponent
+    {
+        string name;
+
+        public Goal(string name)
+        {
+            this.name = name;
+        }
+
+        public void Enter()
+        {
+            Console.WriteLine("enter " + name);
+        }
+
+        public void Process()
+        {
+            Console.WriteLine("process " + name);
+        }
+
+        public void Exit()
+        {
+            Console.WriteLine("exit " + name);
+        }
+    }
+}
---
+++ AI_App/SteeringCS/Goals/GoalComponent.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteeringCS.Goals
+{
+    public abstract class GoalComponent
+    {
+        public void Exit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Process()
+        {
+            throw new NotImplementedException();
+
+        }
+
+        public void Exit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddGoal(GoalComponent goal)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void RemoveGoal(GoalComponent goal)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public string GetGoalName()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

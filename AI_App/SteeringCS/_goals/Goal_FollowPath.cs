0cb928de4839bb81697377ab845a08fc9baa60c9
hassanAlAwwadi <hassan.awwadi@gmail.com>
2019-03-22 17:00:06 +0100

    and this

---
+++ AI_App/SteeringCS/_goals/Goal_FollowPath.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SteeringCS.entity;
+using SteeringCS.Goals;
+
+namespace SteeringCS._goals
+{
+    class Goal_FollowPath : Goal
+    {
+        private Hero _hero;
+
+        public Goal_FollowPath(string name, Hero hero) : base(name)
+        {
+            _hero = hero;
+            started = false;
+            done = false;
+        }
+
+        public override void Enter()
+        {
+            started = true;
+        }
+
+        public override void Process()
+        {
+            
+        }
+
+        public override void Exit()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
